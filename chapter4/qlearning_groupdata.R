ca68b53ac42823fd38070a90a79b4b60da802fbd
Kentaro Katahira <kkatahir@gmail.com>
2018-08-26 20:30:28 +0900

    ソースコード整理

--- chapter4/qlearning_groupdata.R
+++ chapter4/qlearning_groupdata.R
@@ -8,31 +8,32 @@ library(Rsolnp)
 # RStanの読み込み
 require(rstan)
 
-# 以下を実行するとStanのサンプリングを並列化で高速にできる
+# hessianをnumDerivで求める場合
+require(numDeriv)
+
+# 以下を実行するとStanのサンプリングを並列化して実行できる
 # rstan_options(auto_write=TRUE)
 # options(mc.cores=parallel::detectCores())
 
 nseed <- 182
 set.seed(nseed)
 
-flgCompileModel <- 1
-flgHB <- 1
+# 参加者数
+N <- 40 
 
-# Simulation parameters:
-N <- 40             # Number of subjct
-T <-  80            # Number of trials for each subject
+# 各参加者の試行数
+T <-  80   
 
 nParam <- 2
 paramName <- c("alpha", "beta")
 collist <- rep("black",5) 
 pchlist <- c(1, 4, 0)
 
-nModel <- length(smodels)
-
-# Model function (for parameter fit)
 #----------------------------------------------------------#
-# Model function (for individual parameter fit)
-############## ---------------------------------------------#
+# 最尤推定のためのモデル関数の設定
+#----------------------------------------------------------#
+
+# 個人レベルのQ学習モデル
 func_qlearning <- function(param, choice, reward, prior = NULL)
 {
   T <- length(choice)
@@ -40,7 +41,7 @@ func_qlearning <- function(param, choice, reward, prior = NULL)
   beta <- param[2]
   c <- choice
   r <- reward
-  p1 <- numeric(T) 
+  pA <- numeric(T) 
   
   # set Q values (#option x T)
   Q <- matrix(numeric(2*T), nrow=2, ncol=T)
@@ -51,9 +52,9 @@ func_qlearning <- function(param, choice, reward, prior = NULL)
   for (t in 1:T) {
     
     # choosing prob 1
-    p1[t] <- 1/(1+exp(-beta * (Q[1,t]-Q[2,t])))
+    pA[t] <- 1/(1+exp(-beta * (Q[1,t]-Q[2,t])))
     
-    ll <- ll + (c[t]==1) * log(p1[t]) +  (c[t]==2) * log(1-p1[t])
+    ll <- ll + (c[t]==1) * log(pA[t]) +  (c[t]==2) * log(1-pA[t])
     
     # update values 
     if (t < T) {
@@ -73,12 +74,10 @@ func_qlearning <- function(param, choice, reward, prior = NULL)
       dgamma(beta,shape=prior$beta_shape, scale=prior$beta_scale,log = T) 
   }
   
-  return(list(negll = -ll - lprior,Q = Q, p1 = p1))
+  return(list(negll = -ll - lprior,Q = Q, pA = pA))
 }
 
-#----------------------------------------------------------#
-# Model function for fixed effect, pooled group parameter fit
-# ---------------------------------------------#
+# 固定効果のQ学習モデル
 func_qlearning_FE <- function(param, choice, reward)
 {
   
@@ -95,14 +94,14 @@ func_qlearning_FE <- function(param, choice, reward)
   for (idxsub in 1:N) {
     # set Q values (#option x T)
     Q <- matrix(numeric(2*T), nrow=2, ncol=T)
-    p1 <- numeric(T) 
+    pA <- numeric(T) 
     
     for (t in 1:T) {
       
       # choosing prob 1
-      p1[t] <- 1/(1+exp(-beta * (Q[1,t]-Q[2,t])))
+      pA[t] <- 1/(1+exp(-beta * (Q[1,t]-Q[2,t])))
       
-      ll <- ll + (c[idxsub, t]==1) * log(p1[t]) +  (c[idxsub, t]==2) * log(1-p1[t])
+      ll <- ll + (c[idxsub, t]==1) * log(pA[t]) +  (c[idxsub, t]==2) * log(1-pA[t])
       
       # update values 
       if (t < T) {
@@ -117,25 +116,21 @@ func_qlearning_FE <- function(param, choice, reward)
   return(-ll)
 }
 
-#----------------------------------------------------------#
-# objective function to be minimized (for parameter fit) 
-# just pass the negative-likelihood
-#----------------------------------------------------------#
+
 func_minimize <- function(param, choice, reward, prior = NULL)
 {
-  ret = func_qlearning(param, choice, reward, prior)
+  ret <- func_qlearning(param, choice, reward, prior)
   
   # return negative log-likelihood
   return(ret$negll)
 }
 
 #----------------------------------------------------------#
-# Simulation for generating samples
+# Q学習モデルのシミュレーションによるデータ生成
 #----------------------------------------------------------#
-
 c <- matrix(numeric(N * T), nrow = N, ncol = T)
 r <- matrix(numeric(N * T), nrow = N, ncol = T)
-p1 <- numeric(T)
+pA <- numeric(T)
 
 alpha <- rnorm(N, 0.4, 0.2)     # learning rate
 beta <- rnorm(N, 3.0, 1.0)     # inverse temperature
@@ -153,9 +148,9 @@ for (i in 1:N) {
   Q <- matrix(numeric(2 * T), nrow = 2, ncol = T)
   for (t in 1:T) {
     # choosing prob 1
-    p1[t] <- 1 / (1 + exp(-beta[i] * (Q[1, t] - Q[2, t])))
+    pA[t] <- 1 / (1 + exp(-beta[i] * (Q[1, t] - Q[2, t])))
     
-    if (runif(1, 0, 1) < p1[t]) {
+    if (runif(1, 0, 1) < pA[t]) {
       # choose option 1
       c[i, t] <- 1
       r[i, t] <- as.numeric(runif(1, 0, 1) < p[1])
@@ -183,16 +178,20 @@ dataList = list(# Put the information into a list.
   T = T)
 
 #------------------------------------------------------------------------#
-# single subject MLE
+# 個人レベルの最尤推定
 #------------------------------------------------------------------------#
 paramest <- matrix(nrow = N, ncol = nParam)
-paraminvhessian <- matrix(nrow = N, ncol = nParam)
+param_se  <- matrix(nrow = N, ncol = nParam)
 for (idxsub in 1:N) {
+  # 参加者ごとに...
+  
   fval <- Inf
   for (idxopt in 1:10) {
+    
+    # 初期値を一様乱数から生成
     initparam <- runif(nParam, 0, 1.0)
     
-    # solnpで最適化
+    # solnpで負の対数尤度を最小にするパラメータを求める
     res <- solnp(
       initparam,
       fun = func_minimize,
@@ -207,12 +206,15 @@ for (idxsub in 1:N) {
       fval <- res$values[length(res$values)]
       paramest[idxsub, ] <- res$pars
       
-      if (det(res$hessian) > 0.01) {
-        paraminvhessian[idxsub, ] <- diag(solve(res$hessian))
-      } else {
-        paraminvhessian[idxsub, ] <-
-          rep(1, nParam) # diag(solve(res$hessian))
-      }
+      # numDerivパッケージのhessianを使った方がヘッセ行列は精度よく求まる
+      # require(numDeriv)
+      hess <- hessian(func=func_minimize,
+                      x = res$pars,
+                      choice = c[idxsub, ],
+                      reward = r[idxsub, ])
+      
+      # hess <- res$hessian
+      param_se[idxsub, ] <- sqrt(diag(solve(hess)))
       
       cat("\n solnp: ", res$pars, " fval: ", fval)
     }
@@ -221,8 +223,10 @@ for (idxsub in 1:N) {
 SS_param <- list(paramest[, 1], paramest[, 2])
 
 #------------------------------------------------------------------------#
-# single subject MAP
+# 個人レベルのMAP推定
 #------------------------------------------------------------------------#
+
+# 事前分布のパラメータの設定
 prior <-
   list(
     alpha_a = 2,
@@ -230,8 +234,9 @@ prior <-
     beta_shape = 2,
     beta_scale = 3
   )
+
 paramest <- matrix(nrow = N, ncol = nParam)
-paraminvhessian <- matrix(nrow = N, ncol = nParam)
+
 for (idxsub in 1:N) {
   fval <- Inf
   for (idxopt in 1:10) {
@@ -251,15 +256,6 @@ for (idxsub in 1:N) {
     if (fval > res$values[length(res$values)]) {
       fval <- res$values[length(res$values)]
       paramest[idxsub, ] <- res$pars
-      
-      if (0) {
-      if (det(res$hessian) > 0.01) {
-        paraminvhessian[idxsub, ] <- diag(solve(res$hessian))
-      } else {
-        paraminvhessian[idxsub, ] <-
-          rep(1, nParam) # diag(solve(res$hessian))
-      }
-      }
       
       cat("\n solnp: ", res$pars, " fval: ", fval)
     }
@@ -300,18 +296,20 @@ for (idxopt in 1:10) {
 # 階層ベイズモデルの推定
 #------------------------------------------------------------------------#
 
-# initize chains from MLE estimates -------------------- #
+nChains <- 3
+
+# MCMCの初期値を個人レベルの最尤推定値に近くなるよう設定する
 initf <- function(chain_id = 1, SS_param) {
   logit_alpha <-
     log(pmin(SS_param[[1]], 0.9) / (1 - pmin(SS_param[[1]], 0.9)))
   logit_beta <-
     log(pmin(SS_param[[2]] / 20, 0.9) / (1 - pmin(SS_param[[2]] / 20, 0.9)))
-  # on doing
-  mean(logit_alpha)
-  mu_p_alpha = mean(logit_alpha)
+  mu_p_alpha <- mean(logit_alpha)
   sigma_p_alpha = min(sd(logit_alpha), 1.4)
-  mu_p_beta = mean(logit_beta)
-  sigma_p_beta = min(sd(logit_beta), 1.4)
+  mu_p_beta <- mean(logit_beta)
+  sigma_p_beta <- min(sd(logit_beta), 1.4)
+  
+  # リストにまとめて返す
   list(
     alpha = SS_param[[1]],
     beta = SS_param[[2]],
@@ -323,23 +321,18 @@ initf <- function(chain_id = 1, SS_param) {
     eta_beta = (logit_beta - mu_p_beta) / sigma_p_beta
   )
 }
-# generate a list of lists to specify initial values
+# チェインごとの初期値を生成 (この例では全て同じ初期値)
 initsList <- lapply(1:nChains,
                     function(id)
                       initf(chain_id = id, SS_param = SS_param))
 
-# Get MC sample of posterior:
-cat("\n HB ----------")
-
 # run MCMC
-stanFit <- stan(file='model_qlearning_group.stan', 
-                data=dataList, iter=10000, thin=1, 
-                chains=3,
+stanFit <- stan(file = 'model_qlearning_group.stan', 
+                data = dataList, iter = 10000, 
+                thin = 1, 
+                chains = nChains, 
+                warmup = 2000,
                 init = initsList)
-
-#------------------------------------------------------------------------#
-# Evaluating results
-#------------------------------------------------------------------------#
 
 # 個人レベルパラメータのMCMCサンプルを抽出
 param <- c("alpha","beta") # 分析対象とする個人レベルパラメータ
@@ -356,36 +349,36 @@ for (p in 1:length(param)){
   }
 }
 
-# ------------------------------------ #
-# correlation plot SS ML vs HB
-# ------------------------------------ #
-listxylim <- list(c(0,1), c(0,7))
-x11(width = 7*nParam,height = 8)
-par(mfcol=c(1,nParam))
-par(oma=c(1.5,2,2,1))
+#------------------------------------------------------------------------#
+# パラメータの真値と各推定値の相関図
+#------------------------------------------------------------------------#
+
+# 各パラメータのx軸，y軸共通の範囲を設定
+listxylim <- list(c(0,1.05), c(0,7))
+
+# 描画ウィンドウの生成
+x11(width = 7*nParam, height = 8)
+
+# 描画パラメータ
+par(mfcol = c(1,nParam))
+par(oma = c(1.7,2,2,1))
 par(mar = c(4, 4, 4.0, 2))
 
 for (idxparam in 1:nParam) {
   
-  COR_SS[idxparam] <- cor(x=true_param[[idxparam]], 
-                          y = SS_param[[idxparam]])
-  COR_HB[idxparam] <- cor(x=true_param[[idxparam]], 
-                          y = HB_MAP_param[[idxparam]])
-  
-  # x11()
-  maxse <- max(sqrt(paraminvhessian[,idxparam]))
-  minse <- min(sqrt(paraminvhessian[,idxparam]))
-  meanse <- mean(sqrt(paraminvhessian[,idxparam]))
+  maxse <- max(param_se[,idxparam])
+  minse <- min(param_se[,idxparam])
+  meanse <- mean(param_se[,idxparam])
+  sdse <- sd(param_se[,idxparam])
   
   plot(true_param[[idxparam]], SS_param[[idxparam]], 
        pch = 16,
-       col = "gray35",
-       cex = ( (sqrt(paraminvhessian[,idxparam]) - minse) / (maxse - minse) * 
-                 3 + 0.5),
+       col = "gray55",
+       cex = ( (param_se[,idxparam]) / sdse * 0.7 + 0.8),
        main = paste(paramName[idxparam]), 
        las = 1, 
-       xlab = "True parameter",
-       ylab = "Estimates",
+       xlab = "パラメータの真値",
+       ylab = "推定値",
        xlim = listxylim[[idxparam]], 
        ylim = listxylim[[idxparam]], 
        cex.lab  = 1.5,
@@ -397,7 +390,7 @@ for (idxparam in 1:nParam) {
   points(x = true_param[[idxparam]], y= SSMAP_param[[idxparam]], 
          pch = pchlist[2])
   
-  # plot fixed effect parameter
+  # 固定効果分析の推定値を水平線で表示
   abline(h = FE_param[[idxparam]], lty=5)
   
   points(x = true_param[[idxparam]], y= HB_MAP_param[[idxparam]], pch = pchlist[3])
@@ -410,16 +403,7 @@ for (idxparam in 1:nParam) {
   }
   
   abline(a = 0, b = 1, lty = "dashed")
-  # legend("topleft", 
-  #        legend=c(
-  #          sprintf('Single-subject MLE, r = %.3f', 
-  #                  COR_SS[idxparam]), 
-  #          sprintf('Hierarchical Bayes, r = %.3f', 
-  #                  COR_HB[idxparam])
-  #        ),
-  #        bty = "n",
-  #        inset = c(0.02, 0.02),pch = c(pchlist[1],  pchlist[3]))
-  # 
 }
 
-dev.copy2eps(file="./figs/SS_ML_vs_HB_MAP.eps")
+# 図を保存するときは以下を実行
+# dev.copy2eps(file="./figs/SS_ML_vs_HB_MAP.eps")
